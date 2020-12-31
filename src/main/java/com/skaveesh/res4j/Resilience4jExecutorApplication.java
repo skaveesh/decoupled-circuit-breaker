33f60cff722ff29dfe6b899099b7e5979409339d
Samintha Kaveesh <skaveesh@gmail.com>
2020-12-31 13:15:07 +0530

    initial project commit

---
+++ src/main/java/com/skaveesh/res4j/Resilience4jExecutorApplication.java
@@ -0,0 +1,13 @@
+package com.skaveesh.res4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Resilience4jExecutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Resilience4jExecutorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skaveesh/res4j/Resilience4jExecutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skaveesh.res4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Resilience4jExecutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
